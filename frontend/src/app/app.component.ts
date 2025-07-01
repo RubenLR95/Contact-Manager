6683201a81b75e07d1b98277565a73c0b632a3d5
RubénLR95 <ruben.lopez.ras@gmail.com>
2025-07-01 15:52:00 +0200

    Añadido Crud

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    ButtonModule
+    ButtonModule,
+    ContactListComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
+
+  @ViewChild(ContactListComponent) listCmp! : ContactListComponent;
+
+  // onCreate(){             //METODO QUE SE PUEDE LLAMAR AL CREAR UN CONTACTO. RECARGA LA LISTA DE CONTACTOS PARA MOSTRAR EL NUEVO
+  //   this.listCmp.load();
+  // }
+
+  // onUpdated(){            //METODO QUE SE PUEDE LLAMAR CUANDO SE MODIFICA UN CONTACTO. RECARGA LA LISTA Y MUESTRA LOS CAMBIOS
+  //   this.listCmp.load();
+  // }
+
+
 }
---
+++ frontend/src/app/services/contact.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';      //ESTO PERMITE TRABAJAR CON DATOS ASINCRÓNICOS, COMO LAS RESPUESTAS HTTP QUE LLEGAN EN EL FUTURO
+
+// DEFINIMOS LA ESTRUSTURA QUE PUEDE TENER UN CONTACTO
+
+export interface Contact{
+  id: number; // IDENTIFICADOR ÚNICO DEL CONTACTO
+  name: string; // NOMBRE DEL CONTACTO
+  email: string; // CORREO ELECTRÓNICO
+  phone: string; // TELEFONO DELL CONTACTO 
+}
+
+
+
+@Injectable({     // DECORADOR QUE INDICA QUE ESE SERVICIO SE PROVEE A TODA LA APLICACIÓN
+  providedIn: 'root'
+})
+export class ContactService {
+
+  //URL BASE DEL API DONDE SE GESTIONAN LOS CONTACTOS
+  private API = 'http://localhost:3001/contacts';
+
+  constructor(private http: HttpClient) { }          // PERMITE HACER PETICIONES http A UN SERVIDOR BACKEND
+
+  getAll(): Observable<Contact[]>{
+    return this.http.get<Contact[]>(this.API);
+  }
+
+  create(contact: Omit<Contact, 'id'>): Observable<Contact>{  //PERMITE CREAR CONTACTOS
+    return this.http.post<Contact>(this.API, contact);
+  } 
+
+  update(contact: Contact): Observable<Contact>{  //PERMITE MODIFICAR CONTACTOS
+    return this.http.put<Contact>(
+      `${this.API}/${contact.id}`,
+      contact
+    );
+  }
+
+  delete(id: number): Observable<void>{         //PERMITE BORRAR CONTACTOS
+    return this.http.delete<void>(`${this.API}/${id}`);
+  }
+}
